32d25fcd981259b39c49a1acdb1617e2f40056f9
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-05-21 03:53:29 +0900

    use local storage to maintain theme mode on page refreshing

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,12 +1,19 @@
 // ThemeContext.js
 import React from 'react'
-import { createContext, useState, useMemo, useContext } from 'react';
+import { createContext, useState, useMemo, useContext, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-    const [mode, setMode] = useState('light');
+    const [mode, setMode] = useState(() => {
+        const savedMode = localStorage.getItem('themeMode')
+        return savedMode ? savedMode : 'light'
+    });
+
+    useEffect(() => {
+        localStorage.setItem('themeMode', mode)
+    }, [mode])
 
     const theme = useMemo(() => createTheme({
         palette: {
