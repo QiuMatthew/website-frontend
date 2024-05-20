8372aaf926beae63707fb9369f902425bcd38cf5
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-05-21 03:44:22 +0900

    add toggle mode button to homepage and discretelog page for testing

--- src/DiscreteLogPage.js
+++ src/DiscreteLogPage.js
@@ -1,6 +1,13 @@
+import { useThemeContext } from './ThemeContext.js'
+import ToggleColorMode from './ToggleColorMode.js'
 
 export default function DiscreteLogPage() {
+    const { mode, toggleColorMode } = useThemeContext()
+
     return (
+        <>
         <p>This is DiscreteLogPage.</p>
+        <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+        </>
     )
 }
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,13 @@
+import { useThemeContext } from './ThemeContext.js'
+import ToggleColorMode from './ToggleColorMode.js'
 
 export default function HomePage() {
+    const { mode, toggleColorMode } = useThemeContext()
+
     return (
+        <>
         <p>This is HomePage.</p>
+        <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+        </>
     )
 }
