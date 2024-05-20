4da64eff9d0aba0b834188128ec4abdd992b5788
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-05-21 00:22:51 +0900

    create router for pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' exact element={<div><p>Hello</p></div>} />
+                <Route path='/' exact element={<div><p>Hello, this is the homepage</p></div>} />
+                <Route path='/apps/discrete-log' exact element={<div><p>This is the discrete log page</p></div>} />
+                <Route path='/apps/easy-slide' exact element={<div><p>This is the easy slide page</p></div>} />
+                <Route path='/apps/chouseisan' exact element={<div><p>This is the chouseisan page</p></div>} />
 
                 <Route path='*' element={<Navigate to='/' replace />} />
             </Routes>
